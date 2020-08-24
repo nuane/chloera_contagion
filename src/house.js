04b4efebb22a9d757d50a9a4b93905f48b83a60c
marielouie <benjaminkrantz@Benjamins-MacBook-Air.local>
2020-08-23 23:27:50 -0700

    procedural elements mostly done

--- src/house.js
+++ src/house.js
@@ -16,6 +16,7 @@ class House extends Phaser.Scene {
   preload() {
     this.load.image('tiles', 'assets/sample_indoor_kenny.png');
     this.load.image('white_square', 'assets/white_square.png');
+    this.load.image('black_dot', 'assets/black_dot.png');
 
     this.load.tilemapTiledJSON('map', 'assets/sample_indoor1.json');
     // this.load.tilemapTiledJSON('map', 'assets/this.cityGrid.json');
@@ -34,12 +35,15 @@ class House extends Phaser.Scene {
   create(){
     this.input.keyboard.on('keydown', this.handleKey, this);
 
+    this.mapUIOverlay = this.add.text(0, 0, '', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
+    this.mapUIOverlay.depth = 5;
+
     let tileSize = 16;
     const getColor = (zone) => {
       switch (zone) {
-        case 'residential' : return "0x00FF00";
-        case 'commercial' : return "0x0000FF";
-        case 'industrial': return "0xFF0000";
+        case 'residential' : return "0x008080";
+        case 'commercial' : return "0x008080";
+        case 'industrial': return "0xFF69b4";
         case 'road': return "0x303030";
       }
     }
@@ -57,7 +61,7 @@ class House extends Phaser.Scene {
       let validPoint = true;
       while (validPoint){
         let x = Phaser.Math.Between(0, 39);
-        let y = Phaser.Math.Between(0, 25);
+        let y = Phaser.Math.Between(0, 24);
         if (this.cityGrid[y][x].type === 'road')
         {
           this.cityWaterPumps.push(this.cityGrid[y][x]);
@@ -80,23 +84,36 @@ class House extends Phaser.Scene {
         //setting block color here
         cell.sprite = this.add.image(cell.j*tileSize, cell.i*tileSize, 'white_square').setTint(color).setInteractive();
 
+        cell.state = {
+          atRisk: false, infected: false, immune: false,
+          healthy: true,
+        }
+        cell.influenza = 0;
+        cell.poisoned = 0
+
         cell.atRisk = false;
         cell.infected = false;
-        cell.passedAway = false;
         cell.immune = false;
+        cell.timeToHeal = Phaser.Math.Between(10, 20);
 
         cell.health = Phaser.Math.Between(3, 7);
-        cell.timeToHeal = Phaser.Math.Between(5, 15);
+        cell.deaths = [];
+
 
         cell.sprite.on('pointerover', function (event) {
+          this.mapUIOverlay.setText(`${cell.id}`);
           this.cityPlanner[cell.id].forEach(cell => cell.sprite.setAlpha(0.7));
         }, this);
         cell.sprite.on('pointerout', function (event) {
           this.cityPlanner[cell.id].forEach(cell => cell.sprite.clearAlpha());
         }, this);
         cell.sprite.on('pointerdown', function (event) {
-          console.log(this, cell.id);
-        });
+          let test0 = this.cityPlanner[cell.id].filter(cell => cell.infected);
+          // let test1 = this.cityPlanner[cell.id].map(c => c.deaths).reduce((sum, cur) => sum + cur, 0);
+          let test1 = this.cityPlanner[cell.id].map(c => c.deaths);//.reduce((sum, cur) => sum + cur, 0);
+          // console.log(cell, this.cityPlanner[cell.id]);
+          console.log(test1);
+        }, this);
 
       }
     }
@@ -118,6 +135,7 @@ class House extends Phaser.Scene {
     //TODO graphics used for debuggins, not needed for production
     let graphics = this.add.graphics({ lineStyle: { width: 2, color: 0xaa6622 } });
 
+    //iterate through polygon output to set to game logic
     this.waterPumpPolygons = [];
     for (let polygon of polygons){
       let [index, ...polyPoints] = polygon;
@@ -129,16 +147,17 @@ class House extends Phaser.Scene {
 
     let badPump = this.waterPumpPolygons[Phaser.Math.Between(0, this.waterPumpPolygons.length-1)];
     this.allCityCells.forEach(cell => {
-      if (Phaser.Geom.Polygon.Contains(badPump, cell.sprite.x, cell.sprite.y)){
-        //sets district/building as atRisk instead of individual cell in radius of badPump
-        this.cityPlanner[cell.id].forEach(cell => cell.atRisk = true);
-      }
+      //sets district/building as atRisk instead of individual cell in radius of badPump
+      //inificient way to do this since it goes over all the cells multiple times to change the flag
+      if (Phaser.Geom.Polygon.Contains(badPump, cell.sprite.x, cell.sprite.y)) this.cityPlanner[cell.id].forEach(cell => cell.atRisk = true);
 
 
     });
 
     this.timerCounter = 0;
-    this.timedEvent = this.time.addEvent({ delay: 1000, callback: this.onEvent, callbackScope: this, loop: true });
+    this.timedEvent = this.time.addEvent({ delay: 100, callback: this.onEvent, callbackScope: this, loop: true });
+
+
   }
   onEvent(){
 
@@ -150,38 +169,53 @@ class House extends Phaser.Scene {
 
     }
     this.timerCounter++;
-    if (this.timerCounter > 42) this.scene.restart();
+    if (this.timerCounter > 168) this.scene.restart();
 
     for (let y of this.cityGrid) {
       for (let cell of y){
         if  (cell.type !== 'road'){
+
           // console.log(cell);
           let mortalWound = 0;
-          if (cell.passedAway){
-            cell.sprite.setAlpha(0.8);
-            cell.sprite.setTint(0x000000);
-          } else if (cell.immune){
+          if (cell.immune){
+            cell.sprite.setTint(0xFFFFFF);//// TODO
             cell.atRisk = false;
             cell.infected = false;
-          } else if (cell.atRisk){
-            cell.sprite.setAlpha(0.9);
-            cell.atRisk = false;
-            cell.infected = (Math.random() > 0.2) ? true : false;
           } else if (cell.infected){
-            this.cityPlanner[cell.id].forEach(c => c.atRisk = (Math.random() > 0.2) ? true : false);
-            cell.sprite.setTint(0x89a203);
-            mortalWound += (Math.random() > 0.8) ? 3 : 0;
-            cell.timeToHeal -= 1;
-            if (cell.timeToHeal < 0){
-              cell.immune = true;
-              cell.sprite.setTint(0x008080);
-            }
+            // cell.atRisk = false;
+            cell.sprite.setTint(0x89a203);//// TODO
+            //everyone in building block becomes atRisk
+            // this.cityPlanner[cell.id].forEach(c => c.atRisk = (Math.random() > 0.2) ? true : false);
+            // cell.sprite.setTint(0x89a203);//// TODO
+            mortalWound += (Math.random() > 0.6) ? 5 : 0;
+            cell.timeToHeal -= Phaser.Math.Between(1, 3);
+            if (cell.timeToHeal < 0) cell.immune = true;
+          } else if (cell.atRisk){
+            cell.sprite.setTint(0xffa236);//// TODO
+            cell.infected = (Math.random() > 0.98) ? true : false;
+            cell.immune = (Math.random() > 0.995) ? true : false;
           } else {
-            cell.infected = (Math.random() > 0.999 && this.timerCounter < 10) ? true : false;
+            cell.infected = (Math.random() > 0.9994) ? Phaser.Math.Between(3,5) : 0;
           }
-          mortalWound += (Math.random() > 0.995) ? 1 : (Math.random() > 0.9) ? -1 : 0;
+
+          if (cell.influenza > 0) {
+            cell.sprite.setAlpha(0.3);///// TODO
+            if (cell.influenza > 5) this.cityPlanner[cell.id].forEach(c => c.influenza = (Math.random() > 0.2) ? Phaser.Math.Between(3,5) : 0);
+            cell.influenza -= Phaser.Math.Between(2,3);
+            mortalWound += (Math.random > 0.4) ? 1 : 0;
+          } else {
+            cell.sprite.setAlpha(1);///// TODO
+            cell.influenza = (Math.random() > 0.994 && cell.influenza >= 0) ? Phaser.Math.Between(1,7) : 0;
+          }
+          mortalWound += (Math.random() > 0.999) ? 5 : (Math.random() > 0.8) ? -1 : 0;
           cell.health -= mortalWound;
-          if (cell.health <= 0) cell.passedAway = true;
+          if (cell.health <= 0) {
+            let cs = cell.sprite;
+            let counter = this.add.image(cs.x-5 + ((cell.deaths.length%3)*5), cs.y + Math.floor((cell.deaths.length/3)), 'black_dot');
+
+            cell.deaths.push([cell.influenza, cell.infected]);
+            cell.health = Phaser.Math.Between(3,7);
+          }
         }
 
       }
@@ -189,7 +223,8 @@ class House extends Phaser.Scene {
   }
 
   update(time, delta){
-
+    const mInput = this.input.activePointer;
+    this.mapUIOverlay.setPosition(mInput.x, mInput.y);
   }
   handleKey(e) {
     switch(e.code) {
@@ -236,7 +271,7 @@ class House extends Phaser.Scene {
     function Cell(i, j) {
       this.i = i;
       this.j = j;
-      this.type = TYPES.NONE;
+      this.type = TYPES.ROAD;
       this.id = -1;
       this.color = "";
       this.atRisk = false;
@@ -340,7 +375,7 @@ class House extends Phaser.Scene {
     for (var id = 1; id < checkOrder.length; id++) {
       var curTile = checkOrder[id];
 
-      if (curTile.type == TYPES.NONE) {
+      if (curTile.type == TYPES.ROAD) {
         var direction = (Math.random() > .5 ? 1 : 0);
         var square_width = RandomRange(minSize, (direction ? maxSize : minSize));
         var square_height = RandomRange(minSize, (direction ? minSize : maxSize));
